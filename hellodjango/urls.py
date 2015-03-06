d6e58d5acc8d59886e06072453d975805e5045cb
Tran Thanh Hoa <phamhoa.coder@gmail.com>
2015-03-06 15:43:31 +0700

    update index url

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+                       url(r'', include('polls.urls', namespace="polls")),
                        url(r'^polls/', include('polls.urls', namespace="polls")),
                        url(r'^admin/', include(admin.site.urls)),
 )
