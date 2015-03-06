d20c7929e0de5388b6a51e018df6a6bcfb53ed07
Tran Thanh Hoa <phamhoa.coder@gmail.com>
2015-03-06 14:07:06 +0700

    Update content

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, '/polls/templates')]
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'polls', 'templates')]
+STATIC_DIRS = [os.path.join(BASE_DIR, 'polls', 'static')]
 
 
 # Quick-start development settings - unsuitable for production
